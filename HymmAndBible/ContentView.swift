2606b8ade2b29f150cc1a1a254bdfab8e8417f1a
wannabemountainking <kdy0096@gmail.com>
2026-08-30 21:20:16 +0900

    Initial Commit

---
+++ HymmAndBible/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HymmAndBible
+//
+//  Created by YoonieMac on 8/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HymmAndBible/HymmAndBibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HymmAndBibleApp.swift
+//  HymmAndBible
+//
+//  Created by YoonieMac on 8/30/26.
+//
+
+import SwiftUI
+
+@main
+struct HymmAndBibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
