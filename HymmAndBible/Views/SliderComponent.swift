18b659e642967d2147a83a66ed45dea818ba4003
김도윤 <kdy0096@gmail.com>
2026-09-08 21:50:22 +0900

    update2

--- HymmAndBible/Views/SliderComponent.swift
+++ HymmAndBible/Views/SliderComponent.swift
@@ -12,41 +12,34 @@ struct SliderComponent: View {
 	@State private var sliderValue: CGFloat = 0.0
 	
     var body: some View {
-		VStack(spacing: 20) {
-			GeometryReader { geo in
-				let totalSliderWidth = geo.size.width
-				
-				ZStack(alignment: .leading) {
-					// Background
-					Capsule()
-						.fill(Color.gray.opacity(0.5))
-						.frame(height: 10)
-						.frame(maxWidth: .infinity)
-					// Content
-					
-				Capsule()
-					.fill(Color.green.opacity(0.8))
-					.frame(height: 10)
-					.frame(width: totalSliderWidth * sliderValue, height: 12)
-					
-				Circle()
-					.fill(Color.green.opacity(0.8))
-					.frame(height: 12)
-					.offset(x: (totalSliderWidth * sliderValue) - 6)
-					.gesture(
-						DragGesture(minimumDistance: 0.0)
-							.onChanged({ value in
-								let locationX = max(0, min(value.location.x, totalSliderWidth))
-								sliderValue = locationX / totalSliderWidth
-							})
-					)
-				} //:ZSTACK
-				
-				
-				
-			}
-			
-		} //:VSTACK
+        VStack(spacing: 20) {
+            GeometryReader { geo in
+                ZStack(alignment: .leading) {
+                    let totalWidth = geo.size.width
+                    // Background
+                    Capsule()
+                        .foregroundStyle(Color.gray.opacity(0.5))
+                        .frame(height: 12)
+                        .frame(maxWidth: .infinity)
+                    // Content
+                    Circle()
+                        .foregroundStyle(Color.green.opacity(0.8))
+                        .frame(width: 12, height: 12)
+                        .offset(x: sliderValue * totalWidth)
+                        .gesture(
+                            DragGesture(minimumDistance: 0)
+                                .onChanged({ value in
+                                    let locationX = max(6, min(value.location.x, totalWidth - 6))
+                                    sliderValue = locationX / totalWidth
+                                })
+                        )
+                    Capsule()
+                        .foregroundStyle(Color.green.opacity(0.8))
+                        .frame(width: sliderValue * totalWidth + 6, height: 12)
+                } //:ZSTACK
+            } //:GEOMETRY
+            .frame(height: 12)
+        } //:VSTACK
     }
 }
 
